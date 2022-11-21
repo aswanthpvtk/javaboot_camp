62e97da86e66049ccb88f850aa1dc8650f342ce3
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-21 10:44:18 +0530

    main

---
+++ calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        int n1,n2,op;
+
+        System.out.println("enter your option");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("1.Addition\n2.Substraction\n4.Multiplication\n4.Division");
+        op=sc.nextInt();
+
+
+
+
+
+    }
+}

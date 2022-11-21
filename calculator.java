905b1f6103dc89e322461b3a01943b792ff90dc2
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-21 11:15:20 +0530

    mul

--- calculator.java
+++ calculator.java
@@ -3,50 +3,58 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         int n1,n2,op;
+        boolean flag=true;
+     while (flag) {
+         System.out.println("enter your option");
+         Scanner sc = new Scanner(System.in);
+         System.out.println("1.Addition\n2.Substraction\n4.Multiplication\n4.Division\n5.Exit");
+         op = sc.nextInt();
 
-        System.out.println("enter your option");
-        Scanner sc=new Scanner(System.in);
-        System.out.println("1.Addition\n2.Substraction\n4.Multiplication\n4.Division\n5.Exit");
-        op=sc.nextInt();
+         switch (op) {
 
-        switch(op)
-        {
+             case 1:
+                 System.out.println("enter first number");
+                 n1 = sc.nextInt();
+                 System.out.println("enter Secomd number");
+                 n2 = sc.nextInt();
+                 int a = n1 = n2;
+                 System.out.println("after addition:" + a);
 
-            case 1:
-                System.out.println("enter first number");
-                n1=sc.nextInt();
-                System.out.println("enter Secomd number");
-                n2=sc.nextInt();
-                int a=n1=n2;
-                System.out.println("after addition:"+a);
-
-            case 2:
-                System.out.println("enter first number");
-                n1=sc.nextInt();
-                System.out.println("enter Secomd number");
-                n2=sc.nextInt();
-                int b=n1-n2;
-                System.out.println("after substraction:"+b);
+             case 2:
+                 System.out.println("enter first number");
+                 n1 = sc.nextInt();
+                 System.out.println("enter Secomd number");
+                 n2 = sc.nextInt();
+                 int b = n1 - n2;
+                 System.out.println("after substraction:" + b);
 
 
+             case 3:
+                 System.out.println("enter first number");
+                 n1 = sc.nextInt();
+                 System.out.println("enter Secomd number");
+                 n2 = sc.nextInt();
+                 int c = n1 * n2;
+                 System.out.println("after Multiplication:" + c);
 
-            case 3:
-                System.out.println("enter first number");
-                n1=sc.nextInt();
-                System.out.println("enter Secomd number");
-                n2=sc.nextInt();
-                int c=n1*n2;
-                System.out.println("after Multiplication:"+c);
+             case 4:
+                 System.out.println("enter first number");
+                 n1 = sc.nextInt();
+                 System.out.println("enter Secomd number");
+                 n2 = sc.nextInt();
+                 int d = n1 / n2;
+                 System.out.println("after division:" + d);
 
-            case 4:
-                System.out.println("enter first number");
-                n1=sc.nextInt();
-                System.out.println("enter Secomd number");
-                n2=sc.nextInt();
-                int d=n1/n2;
-                System.out.println("after division:"+d);
-        }
+             case 5:
+                 flag=false;
 
+
+             default: {
+                 System.out.println("no option selected");
+             }
+
+         }
+     }
 
 
 

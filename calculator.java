d2b14ffdc8aefcc12882a99e56f175b46a80d382
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-21 11:00:51 +0530

    mul

--- calculator.java
+++ calculator.java
@@ -6,18 +6,37 @@ public class calculator {
 
         System.out.println("enter your option");
         Scanner sc=new Scanner(System.in);
-        System.out.println("1.Addition\n2.Substraction\n4.Multiplication\n4.Division");
+        System.out.println("1.Addition\n2.Substraction\n4.Multiplication\n4.Division\n5.Exit");
         op=sc.nextInt();
 
         switch(op)
         {
+
+            case 1:
+                System.out.println("enter first number");
+                n1=sc.nextInt();
+                System.out.println("enter Secomd number");
+                n2=sc.nextInt();
+                int a=n1=n2;
+                System.out.println("after addition:"+a);
+
             case 2:
                 System.out.println("enter first number");
                 n1=sc.nextInt();
                 System.out.println("enter Secomd number");
                 n2=sc.nextInt();
-                int a=n1-n2;
-                System.out.println("after Substraction :"+a);
+                int b=n1-n2;
+                System.out.println("after substraction:"+b);
+
+
+
+            case 3:
+                System.out.println("enter first number");
+                n1=sc.nextInt();
+                System.out.println("enter Secomd number");
+                n2=sc.nextInt();
+                int c=n1*n2;
+                System.out.println("after Multiplication:"+c);
         }
 
 
